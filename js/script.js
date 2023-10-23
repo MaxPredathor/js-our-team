993d9d8fc273967647b30799eeecb265809a0d24
MaxPredathor <max.gilli98@gmail.com>
2023-10-23 15:08:35 +0200

    added printing function

--- js/script.js
+++ js/script.js
@@ -1,68 +1,63 @@
 "use strict";
 
-(function(){
+const ourTeam = [
+    {
+        name: "Wayne Barnett",
+        position: "Founder & CEO",
+        picture: "wayne-barnett-founder-ceo.jpg"
+    },
+    
+    {
+        name: "Angela Caroll",
+        position: "Chief Editor",
+        picture: "angela-caroll-chief-editor.jpg"
+    },
 
-    const row = document.querySelector(".row");
-    const ourTeam = [
-        {
-            name: 'Wayne Barnett',
-            position: 'Founder & CEO',
-            picture: 'wayne-barnett-founder-ceo.jpg'
-        },
+    {
+        name: "Walter Gordon",
+        position: "Office Manager",
+        picture: "walter-gordon-office-manager.jpg"
+    },
+    
+    {
+        name: "Angela Lopez",
+        position: "Social Media Manager",
+        picture: "angela-lopez-social-media-manager.jpg"
+    },
+    
+    {
+        name: "Scott Estrada",
+        position: "Developer",
+        picture: "scott-estrada-developer.jpg"
+    },
+    
+    {
+        name: "Barbara Ramos",
+        position: "Graphic Designer",
+        picture: "barbara-ramos-graphic-designer.jpg"
+    },
+];
+console.log(ourTeam);
+const row = document.querySelector(".row");
 
-        {
-            name: 'Angela Caroll',
-            position: 'Chief Editor',
-            picture: 'angela-caroll-chief-editor.jpg'
-        },
+for(const val in ourTeam){
+    printCol(ourTeam[val]);
+}
 
-        {
-            name: 'Walter Gordon',
-            position: 'Office Manager',
-            picture: 'walter-gordon-office-manager.jpg'
-        },
-
-        {
-            name: 'Angela Lopez',
-            position: 'Social Media Manager',
-            picture: 'angela-lopez-social-media-manager.jpg'
-        },
-
-        {
-            name: 'Scott Estrada',
-            position: 'Developer',
-            picture: 'scott-estrada-developer.jpg'
-        },
-
-        {
-            name: 'Barbara Ramos',
-            position: 'Graphic Designer',
-            picture: 'barbara-ramos-graphic-designer.jpg'
-        },
-    ];
-    console.log(ourTeam);
-
-    for(let i of ourTeam){
-        printCol(ourTeam[i]);
-    }
-
-    function printCol(ourTeam){
-        const col = document.createElement("div");
-        col.classList.add("col-4");
-        const schema = 
-        `
-        <div class="card text-start">
-          <img class="card-img-top" src="../img/${ourTeam.picture}" alt="Card">
-          <div class="card-body">
+function printCol(ourTeam){
+    const col = document.createElement("div");
+    col.classList.add("col-4");
+    const schema = 
+    `
+    <div class="card text-start">
+        <img class="card-img-top" src="img/${ourTeam.picture}">
+        <div class="card-body">
             <h3 class="card-title">${ourTeam.name}</h3>
             <p class="card-text">${ourTeam.position}</p>
-            </div>
-        </div>  
-        `;
-        col.innerHTML = schema;
-        row.append(col);
-    }
-
-
-}) ();
+        </div>
+    </div>  
+    `;
+    col.innerHTML = schema;
+    row.append(col);
+}
 
